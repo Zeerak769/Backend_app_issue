84925661d9ca7d1b67bd84c238661e6f534392f9
Zeerak769 <zeeraknoumansiddiqui000@gmail.com>
2024-09-16 17:23:56 +0500

    initial commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: ['http://localhost:5000/api'] });
 
 export const login = (formData) => API.post('/auth/login', formData);
 export const register = (formData) => API.post('/auth/register', formData);
